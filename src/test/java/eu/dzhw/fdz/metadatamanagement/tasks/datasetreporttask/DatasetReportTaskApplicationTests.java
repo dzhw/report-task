031044287c14a72d95f1797560ba12307f048486
René Reitmann <reitmann@dzhw.eu>
2019-06-28 14:38:55 +0200

    dzhw/metadatamanagement#2265 add spotify docker plugin to the build

--- src/test/java/eu/dzhw/fdz/metadatamanagement/tasks/datasetreporttask/DatasetReportTaskApplicationTests.java
+++ src/test/java/eu/dzhw/fdz/metadatamanagement/tasks/datasetreporttask/DatasetReportTaskApplicationTests.java
@@ -6,15 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Smoketest for this task.
+ * Smoketest for this task. Runs the task for one dataset against a running instance of the MDM.
  *
  * @author René Reitmann
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DatasetReportTaskApplicationTests {
-  
+
   @Test
-  public void contextLoads() {}
+  public void taskRuns() {}
 
 }
