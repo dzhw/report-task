4efa1128952bf30821d37a4ce0a530a931e4084d
René Reitmann <reitmann@dzhw.eu>
2019-07-05 12:44:32 +0200

    dzhw/metadatamanagement#2265 make maven quiet

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,7 +10,7 @@ PASSWORD="$2"
 TRAVIS_BRANCH="$3"
 DOCKER_USER="$4"
 DOCKER_PASSWORD="$5"
-mvn --batch-mode --no-transfer-progress dockerfile:push dockerfile:push@push-image-latest -Ddockerfile.username=$DOCKER_USER -Ddockerfile.password=$DOCKER_PASSWORD
+mvn --quiet dockerfile:push dockerfile:push@push-image-latest -Ddockerfile.username=$DOCKER_USER -Ddockerfile.password=$DOCKER_PASSWORD
 if [ $? -ne 0 ]; then
     echo "docker push failed!"
     exit -1
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     echo "cf login failed!"
     exit -1
 fi
-VERSION=$(mvn --batch-mode --no-transfer-progress help:evaluate -Dexpression=project.version -q -DforceStdout)
+VERSION=$(mvn --quiet help:evaluate -Dexpression=project.version -q -DforceStdout)
 cf push -f deploy/manifest.yml --var version=${VERSION}
 if [ $? -ne 0 ]; then
     echo "cf push failed!"
