363bf985c27108dc9b939ec6b398d7f563c5a020
René Reitmann <reitmann@dzhw.eu>
2020-01-17 12:10:40 +0100

    Remove cloudfoundry from task deployment (dzhw/metadatamanagement#2465)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
-# this script deploys the project with a given profile to the correct space in
-# cloudfoundry
+# this script deploys the container image to Amazon ECR
 if [[ $0 != ./deploy/* ]]; then
   echo "Please run this script from the root of our repository!"
   exit -1
 fi
-USERNAME="$1"
-PASSWORD="$2"
-TRAVIS_BRANCH="$3"
 $(aws ecr get-login --no-include-email --region eu-central-1 --profile mdm)
 mvn dockerfile:push dockerfile:push@push-image-latest
 if [ $? -ne 0 ]; then
     echo "docker push failed!"
-    exit -1
-fi
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  PROFILE="prod"
-fi
-if [ "${TRAVIS_BRANCH}" = "test" ]; then
-  PROFILE="test"
-fi
-if [ -z ${PROFILE} ]; then
-  PROFILE="dev"
-fi
-if [ -z ${USERNAME} ] || [ -z ${PASSWORD} ]; then
-  cf login -o DZHW -s ${PROFILE} -a https://api.run.pivotal.io
-else
-  cf login -o DZHW -s ${PROFILE} -u ${USERNAME} -p ${PASSWORD} -a https://api.run.pivotal.io
-fi
-if [ $? -ne 0 ]; then
-    echo "cf login failed!"
-    exit -1
-fi
-VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-cf push -f deploy/manifest.yml --var version=${VERSION}
-if [ $? -ne 0 ]; then
-    echo "cf push failed!"
     exit -1
 fi
