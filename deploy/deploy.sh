6951157faba292283a7d03951faeaaa6ef1f1779
René Reitmann <reitmann@dzhw.eu>
2020-01-20 11:28:52 +0100

    use sts assume role (dzhw/metadatamanagement#2465)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,11 @@ if [[ $0 != ./deploy/* ]]; then
   echo "Please run this script from the root of our repository!"
   exit -1
 fi
-$(aws ecr get-login --no-include-email --region eu-central-1 --profile mdm)
+AWS_CRED=($(aws sts assume-role --role-arn arn:aws:iam::347729458675:role/Admin --role-session-name travis-deployment --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text))
+export AWS_ACCESS_KEY_ID=${AWS_CRED[0]}
+export AWS_SECRET_ACCESS_KEY=${AWS_CRED[1]}
+export AWS_SESSION_TOKEN=${AWS_CRED[2]}
+$(aws ecr get-login --no-include-email --region eu-central-1)
 mvn dockerfile:push dockerfile:push@push-image-latest
 if [ $? -ne 0 ]; then
     echo "docker push failed!"
